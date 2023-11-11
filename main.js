e3bf0ecd87011da75e16c453c2f0c5d8f371d8a6
aciken <adrianmarton2006@gmail.com>
2023-11-11 17:17:16 +0100

    made anoter calc version

--- main.js
+++ main.js
@@ -4,69 +4,76 @@ const operation = document.querySelectorAll('.operation');
 const equal = document.querySelector('.equal');
 const numbersArray = Array.from(numbers);
 
-let numberIndex ='';
-let number1 = '';
-let number2 = ''
-let whatOperation ='';
-let result
-let num1;
-let num2;
-let whatLength = '';
+let screenView = '';
+let numSave = '';
+let operationSave= '';
+let nums = []
+let num1 = '';
+let num2 = '';
+let click = 0;
+let result =''
+let punctLocation = ''
 
-const makeNumber = (numbers) => {
-    numbers.forEach(number => {
-      number.addEventListener('click', (e) => {
-        numberIndex = screen.textContent
-        numberIndex += e.target.textContent;
-        screen.textContent = numberIndex;
-            number1 = numberIndex;
-            console.log(number1)
-      });
-    });
-  };
-  
-  makeNumber(numbersArray);
+const getNumbers = (array =>{
+  array.forEach(element =>{
+    element.addEventListener('click', (e) =>{
+      screenView += e.target.textContent;
+      numSave += e.target.textContent;
+      screen.textContent = screenView;
+    })
+  })
+})
+
+getNumbers(numbersArray);
+
+const saveWhenOp = (e) =>{
+
+  operationSave = e.target.textContent;
+  const stepOne = screenView.split('');
+  num1= numSave;
+  punctLocation = stepOne.length-1;
+  if(stepOne[stepOne.length-1] === '+'||stepOne[stepOne.length-1] === '-'||stepOne[stepOne.length-1] === 'X'||stepOne[stepOne.length-1] === '/'){
+    const stepTwo = stepOne.splice(stepOne.length-1, 1, operationSave)
+    screenView = stepTwo.join('');
+    console.log(screenView);
+    screen.textContent = screenView
+  }else
+  stepOne.push(operationSave);
+  screenView = stepOne.join('');
+  console.log(screenView);
+  screen.textContent = screenView;
+}
 
 
+operation.forEach(element =>{
+  element.addEventListener('click', saveWhenOp)
 
-    operation.forEach(element =>{
-        element.addEventListener('click', (e) =>{
-            num1 = Number(number1);
-            whatLength = number1.length;
-            number1 = ''
-            numberIndex = ''
-            whatOperation = e.target.textContent;
-            numberIndex += e.target.textContent
-            screen.textContent += numberIndex; 
-            console.log(numberIndex)
-            
-                  
-        })
 })
 
 equal.addEventListener('click', () =>{
-   num2 = Number(number1.slice(whatLength+1, number1.length));
-   console.log(whatLength)
-   console.log(num1 ,num2)
-   console.log(whatOperation)
-   if(whatOperation === '+'){
-      result = num1 + num2
-   } else if(whatOperation === '-'){
-    result = num1 - num2
-   } else if(whatOperation === 'X'){
-    result = num1 * num2
-   } else result = num1 / num2;
-
-
-    // switch(whatOperation){
-    //     case'+': result = num1 + num2;break;
-    //     case'-': result = num1 - num2;break;
-    //     case'*': result = num1 * num2;break;
-    //     case'/': result = num1 / num2;break;
-    // }
-    screen.textContent = result;
-    num1 = result;
+  const stepOne = numSave.split('');
+  console.log(stepOne)
+  const stepTwo = stepOne.splice(punctLocation+1, stepOne.length)
+  console.log(punctLocation+1, stepOne.length-punctLocation+1)
+  console.log(stepTwo)
+  num2 = stepTwo.join('');
+  console.log(num2)
+  
+  if(operationSave === '+'){
+    result = Number(num1) + Number(num2);
+  } else if(operationSave === '-'){
+    result = Number(num1) - Number(num2);
+  } else if(operationSave === 'X'){
+    result = Number(num1) * Number(num2);
+  } else result = Number(num1) / Number(num2);
+  screen.textContent = result;
 })
+
+
+
+
+
+
 
 
 
