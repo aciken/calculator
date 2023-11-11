2dfd32d3e0d06f4641759a84a50235a87277e61d
aciken <adrianmarton2006@gmail.com>
2023-11-11 17:25:42 +0100

    operation change works

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ let num1 = '';
 let num2 = '';
 let click = 0;
 let result =''
-let punctLocation = ''
 
 const getNumbers = (array =>{
   array.forEach(element =>{
@@ -31,7 +30,6 @@ const saveWhenOp = (e) =>{
   operationSave = e.target.textContent;
   const stepOne = screenView.split('');
   num1= numSave;
-  punctLocation = stepOne.length-1;
   if(stepOne[stepOne.length-1] === '+'||stepOne[stepOne.length-1] === '-'||stepOne[stepOne.length-1] === 'X'||stepOne[stepOne.length-1] === '/'){
     const stepTwo = stepOne.splice(stepOne.length-1, 1, operationSave)
     screenView = stepTwo.join('');
@@ -51,14 +49,12 @@ operation.forEach(element =>{
 })
 
 equal.addEventListener('click', () =>{
-  const stepOne = numSave.split('');
-  console.log(stepOne)
-  const stepTwo = stepOne.splice(punctLocation+1, stepOne.length)
-  console.log(punctLocation+1, stepOne.length-punctLocation+1)
-  console.log(stepTwo)
-  num2 = stepTwo.join('');
+  num2 = numSave
+                .split('')
+                .slice(num1.length, numSave.length)
+                .join('')
+
   console.log(num2)
-  
   if(operationSave === '+'){
     result = Number(num1) + Number(num2);
   } else if(operationSave === '-'){
